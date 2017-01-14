bc5bf0efe97efd43cb1c82198530d2cc18fc4dc1
Leonid Gordo <leonardpt@gmail.com>
2017-01-14 11:47:30 +0300

    PropertyEqualityComparer now allows null members

--- src/Functional.Fluent/PropertyEqualityComparer.cs
+++ src/Functional.Fluent/PropertyEqualityComparer.cs
@@ -18,9 +18,17 @@ namespace Functional.Fluent
             _accessor = GetAccessor(selector);
         }
 
-        public bool Equals(T x, T y) => _accessor(x).Zip(_accessor(y), Tuple.Create).All(v => v.Item1.Equals(v.Item2));
+        public bool Equals(T x, T y) =>
+           _accessor(x).Zip(_accessor(y), Tuple.Create).All(v => EqualsInternal(v.Item1, v.Item2));
 
-        public int GetHashCode(T obj) => _accessor(obj).Aggregate(0, (a, v) => a ^ v.GetHashCode());
+        private bool EqualsInternal(object x, object y)
+        {
+            if (x == null && y == null) return true;
+            if (x == null || y == null) return false;
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(T obj) => _accessor(obj).Aggregate(0, (a, v) => a ^ (v?.GetHashCode() ?? 0));
 
         private Func<T, IEnumerable<object>> GetAccessor(Expression<Func<T, V>> selector) =>
             selector.Body.ToM().TypeMatch()
