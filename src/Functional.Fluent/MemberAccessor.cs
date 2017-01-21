3f051b40806fe14c485364b0d5ac0e414f2dc8b3
Leonid Gordo <leonardpt@gmail.com>
2017-01-21 23:33:32 +0300

    Member accessor now uses ElseThrow methods

--- src/Functional.Fluent/MemberAccessor.cs
+++ src/Functional.Fluent/MemberAccessor.cs
@@ -18,41 +18,26 @@ namespace Functional.Fluent
 
         public V GetValue(T obj)
         {
-            var mi = MemberInfo;
-            if (mi == null)
-                throw new InvalidOperationException();
-            return mi.TypeMatch()
+            return MemberInfo.TypeMatch()
                 .With(Case.Is<PropertyInfo>(), v => GetValue(v, obj))
                 .With(Case.Is<FieldInfo>(), v => GetValue(v, obj))
-                .Else(_ => ThrowGetException())
+                .ElseThrow<InvalidOperationException>()
                 .Do();
         }
 
         public void SetValue(T obj, V value)
         {
-            var mi = MemberInfo;
-            if (mi == null)
-                throw new InvalidOperationException();
-            mi.TypeMatch()
+           MemberInfo.TypeMatch()
                 .With(Case.Is<PropertyInfo>(), v => SetValue(v, obj, value))
                 .With(Case.Is<FieldInfo>(), v => SetValue(v, obj, value))
-                .Else(_ => ThrowSetException())
+                .ElseThrow<InvalidOperationException>()
                 .Do();
         }
 
         private MemberInfo MemberInfo => _memberExpression.Body.TypeMatch()
                 .With(Case.Is<MemberExpression>(), v => v.Member)
+                .ElseThrow<InvalidOperationException>()
                 .Do();
-
-        private V ThrowGetException()
-        {
-            throw new InvalidOperationException();
-        }
-
-        private IResult ThrowSetException()
-        {
-            throw new InvalidOperationException();
-        }
 
         private V GetValue(PropertyInfo pi, T obj) => (V)pi.GetValue(obj);
 
