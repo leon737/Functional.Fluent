7c001756bd316658faacb6855c72350c8b4c341c
Leonid Gordo <leonardpt@gmail.com>
2017-03-07 22:43:21 +0300

    bound values improvements

--- src/FluentTests/BoundTests.cs
+++ src/FluentTests/BoundTests.cs
@@ -8,6 +8,8 @@ namespace FluentTests
     [TestFixture]
     public class BoundTests
     {
+
+        [Test]
         public void TestBoundExactValues()
         {
             var boundType = new BoundType<int>().Add(new EqualsConstraint<int>(5)).Add(new EqualsConstraint<int>(10));
@@ -17,7 +19,24 @@ namespace FluentTests
             Assert.AreEqual(5, bound.Value);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => new Bound<int>(7, boundType));
+        }
 
+        [Test]
+        public void TestBoundExactValuesAllContraints()
+        {
+            var boundType = new BoundType<int>(true).Add(new EqualsConstraint<int>(5)).Add(new EqualsConstraint<int>(10));
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Bound<int>(5, boundType));
+        }
+
+        [Test]
+        public void TestBoundExactValuesAllContraints2()
+        {
+            var boundType = new BoundType<int>(true).Add(new NotEqualsConstraint<int>(5)).Add(new NotEqualsConstraint<int>(10));
+
+            var bound = new Bound<int>(7, boundType);
+            
+            Assert.AreEqual(7, bound.Value);
         }
 
         [Test]
@@ -34,7 +53,41 @@ namespace FluentTests
 
             Assert.Throws<ArgumentOutOfRangeException>(() => bound.Set(4));
             Assert.Throws<ArgumentOutOfRangeException>(() => bound.Set(11));
+        }
 
+        [Test]
+        public void TestBoundExactValuesExplicitCheck()
+        {
+            var boundType = new BoundType<int>().Add(new EqualsConstraint<int>(5)).Add(new EqualsConstraint<int>(10)).OnExplicitCheck();
+
+            var bound = new Bound<int>(5, boundType);
+
+            Assert.AreEqual(5, bound.Value);
+            Assert.True(bound.IsValid);
+
+            bound = new Bound<int>(7, boundType);
+            Assert.False(bound.IsValid, "bound.IsValid");
+        }
+
+        [Test]
+        public void TestBoundRangeValuesExplicitCheck()
+        {
+            var bound = new BoundType<int>().Add(new RangeConstraint<int>(5, 10)).OnExplicitCheck().Create(5);
+            Assert.AreEqual(5, bound.Value);
+
+            for (int i = 6; i < 11; i++)
+            {
+                bound = bound.Set(i);
+                Assert.AreEqual(i, bound.Value);
+            }
+
+            bound = bound.Set(4);
+            Assert.False(bound.IsValid, "bound.IsValid");
+            Assert.AreNotEqual(10, bound.Value);
+
+            bound = bound.Set(11);
+            Assert.False(bound.IsValid, "bound.IsValid");
+            Assert.AreNotEqual(10, bound.Value);
         }
     }
 }
--- src/Functional.Fluent/MonadicTypes/Bound.cs
+++ src/Functional.Fluent/MonadicTypes/Bound.cs
@@ -1,66 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Functional.Fluent.MonadicTypes
 {
-    public class BoundType<T>: IBoundType<T>
+    internal enum BoundTypeBehaviour
+    {
+        Exception,
+        ExplicitCheck
+    }
+
+
+    public class BoundType<T> //: IBoundType<T>
     {
         private readonly IList<IConstraint<T>> _constraints = new List<IConstraint<T>>();
+        private readonly Func<IEnumerable<IConstraint<T>>, Func<IConstraint<T>, bool>, bool> _func;
 
-        public IBoundType<T> Add(IConstraint<T> constraint)
+        public BoundType()
+        {
+            _func = Enumerable.Any;
+        }
+
+        public BoundType(bool requireAllContraints)
+        {
+            _func = requireAllContraints ? (Func<IEnumerable<IConstraint<T>>, Func<IConstraint<T>, bool>, bool>)Enumerable.All : Enumerable.Any;
+        }
+
+        public BoundType<T> Add(IConstraint<T> constraint)
         {
             _constraints.Add(constraint);
             return this;
         }
 
-        public bool Check(T value)
-        {
-            foreach (var constraint in _constraints)
-            {
-                if (!constraint.IsValid(value)) return false;
-            }
-            return true;
-        }
+        public bool Check(T value) => _func(_constraints, v => v.IsValid(value));
 
         public IBound<T> Create(T value)
         {
             return new Bound<T>(value, this);
         }
-    }
 
-    public interface IBoundType<T>
-    {
-        IBoundType<T> Add(IConstraint<T> constraint);
+        public BoundType<T> OnException()
+        {
+            Behaviour = BoundTypeBehaviour.Exception;
+            return this;
+        }
 
-        bool Check(T value);
+        public BoundType<T> OnExplicitCheck()
+        {
+            Behaviour = BoundTypeBehaviour.ExplicitCheck;
+            return this;
+        }
 
-        IBound<T> Create(T value);
-
+        internal BoundTypeBehaviour Behaviour { get; private set; } = BoundTypeBehaviour.Exception;
     }
 
     public interface IBound<T>
     {
         T Value { get; }
+
         IBound<T> Set(T value);
+
+        bool IsValid { get; }
     }
 
 
     public class Bound<T> : MonadicValue<T>, IBound<T>
     {
-        private readonly IBoundType<T> _boundType;
+        private readonly BoundType<T> _boundType;
 
-        public Bound(T value, IBoundType<T> boundType)
+        public bool IsValid { get; }
+
+
+        public Bound(T value, BoundType<T> boundType)
         {
             _boundType = boundType;
-            SetValue(value);
+            IsValid = SetValue(value);
         }
 
-        private void SetValue(T value)
+        private bool SetValue(T value)
         {
             if (CheckValue(value))
+            {
                 WrappedValue = value;
-            else
+                return true;
+            }
+            if (_boundType.Behaviour == BoundTypeBehaviour.Exception)
                 throw new ArgumentOutOfRangeException(nameof(value));
+            return false;
         }
 
         private bool CheckValue(T value)
@@ -68,18 +94,15 @@ namespace Functional.Fluent.MonadicTypes
             return _boundType.Check(value);
         }
 
-        public new T Value
-        {
-            get { return WrappedValue; }
-        }
+        public new T Value => WrappedValue;
 
         public IBound<T> Set(T value)
         {
-           return new Bound<T>(value, _boundType);
+            return new Bound<T>(value, _boundType);
         }
     }
 
-    
+
     public interface IConstraint<in T>
     {
         bool IsValid(T value);
@@ -94,10 +117,26 @@ namespace Functional.Fluent.MonadicTypes
         {
             _value = value;
         }
-        
+
         public bool IsValid(T value)
         {
             return _value.Equals(value);
+        }
+    }
+
+    public class NotEqualsConstraint<T> : IConstraint<T>
+        where T : IEquatable<T>
+    {
+        private readonly T _value;
+
+        public NotEqualsConstraint(T value)
+        {
+            _value = value;
+        }
+
+        public bool IsValid(T value)
+        {
+            return !_value.Equals(value);
         }
     }
 
@@ -117,6 +156,22 @@ namespace Functional.Fluent.MonadicTypes
         }
     }
 
+    public class LessConstraint<T> : IConstraint<T>
+        where T : IComparable<T>
+    {
+        private readonly T _value;
+
+        public LessConstraint(T value)
+        {
+            _value = value;
+        }
+
+        public bool IsValid(T value)
+        {
+            return _value.CompareTo(value) > 0;
+        }
+    }
+
     public class GreaterOrEqualsConstraint<T> : IConstraint<T>
        where T : IComparable<T>
     {
@@ -133,21 +188,43 @@ namespace Functional.Fluent.MonadicTypes
         }
     }
 
+    public class GreaterConstraint<T> : IConstraint<T>
+       where T : IComparable<T>
+    {
+        private readonly T _value;
+
+        public GreaterConstraint(T value)
+        {
+            _value = value;
+        }
+
+        public bool IsValid(T value)
+        {
+            return _value.CompareTo(value) < 0;
+        }
+    }
+
     public class RangeConstraint<T> : IConstraint<T>
       where T : IComparable<T>
     {
         private readonly T _min;
         private readonly T _max;
+        private readonly bool _includeMin;
+        private readonly bool _includeMax;
 
-        public RangeConstraint(T min, T max)
+
+        public RangeConstraint(T min, T max, bool includeMin = true, bool includeMax = true)
         {
             _min = min;
             _max = max;
+            _includeMin = includeMin;
+            _includeMax = includeMax;
         }
 
         public bool IsValid(T value)
         {
-            return _min.CompareTo(value) <= 0 && _max.CompareTo(value) >= 0;
+            return (_includeMin ? _min.CompareTo(value) <= 0 : _min.CompareTo(value) < 0)
+                && (_includeMax ? _max.CompareTo(value) >= 0 : _max.CompareTo(value) > 0);
         }
     }
 }
