22c4ee76855c6bf14dabff0ca5e75a9d50022ee6
Leonid Gordo <leonardpt@gmail.com>
2017-03-31 22:38:04 +0300

    MakeResult for functions without input parameters

--- src/Functional.Fluent/Helpers/Funcs.cs
+++ src/Functional.Fluent/Helpers/Funcs.cs
@@ -1,7 +1,19 @@
-﻿namespace Functional.Fluent.Helpers
+﻿using System;
+using Functional.Fluent.MonadicTypes;
+
+namespace Functional.Fluent.Helpers
 {
     public static partial class Funcs
     {
-        public static MonadicTypes.FuncState<T> Get<T>() => new MonadicTypes.FuncState<T>();
+        public static FuncState<T> Get<T>() => new FuncState<T>();
+
+        public delegate bool OutParamsDelegateNoInputParameters<R>(out R result);
+
+        public static Func<Result<R>> MakeResult<R>(OutParamsDelegateNoInputParameters<R> d) => () =>
+        {
+            R result;
+            return d(out result) ? Result.Success(result) : Result.Fail<R>();
+        };
+
     }
 }
